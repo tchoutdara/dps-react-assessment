e8f5d03f44c35eb87be307b23143f954363ff59b
tchoutdara <monkeymilitia444@gmail.com>
2018-10-08 09:10:45 -0600

    add scrolling for breweries

--- client/src/components/Beers.js
+++ client/src/components/Beers.js
@@ -1,17 +1,16 @@
 // Note: Started this componenet around 11AM finally beers to render around 1PM
 import React from 'react'
 import axios from 'axios'
-import { Container, Card, Header, Divider } from 'semantic-ui-react'
+import { Container, Card, Header, Divider, Button } from 'semantic-ui-react'
+import InfiniteScroll from 'react-infinite-scroller'
 
 class Beers extends React.Component {
-  state = { beers: [] }
+  state = { beers: [], page: 1, totalPages: 0 }
 
 componentDidMount() {
     axios.get('/api/all_beers')
-    .then( res => this.setState({ beers: res.data.entries }) )
+    .then( res => this.setState({ beers: res.data.entries, totalPages: res.data.total_pages }) )
 }
-
-
 
 showBeers = () => {
   return this.state.beers.map( (beer, i) => {
@@ -21,20 +20,43 @@ showBeers = () => {
         <Card.Header>
             {beer.name}
         </Card.Header>
+        <Card.Description>
+            Description: {beer.description}
+        </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          {/* Add link to button to show beer details page */}
+          <Button basic fluid color='green'> View Beer</Button>
         </Card.Content>
     </Card>
     )
   })
 }
 
+moreBeersPlz = () => {
+    axios.get(`/api/all_beers?page=${this.state.page + 1}`)
+      .then( ({ data }) => {
+        this.setState( state => {
+          return { beers: [...state.beers, ...data.entries], page: state.page + 1  }
+        })
+      })
+   }
+
   render() {
     return(
       <Container textAlign='center'>
       <Divider />
         <Header as='h1' color='green'>Beer List</Header>
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this.moreBeersPlz}
+          hasMore={this.state.page < this.state.totalPages}
+          loader={<div className='loader' key={0}>Loading ...</div>}
+        >
         <Card.Group itemsPerRow={4}>
           {this.showBeers()}
         </Card.Group>
+        </InfiniteScroll>
       </Container>
       )
     }
--- client/src/components/Breweries.js
+++ client/src/components/Breweries.js
@@ -1,17 +1,16 @@
 // Notes: stated this component around 1:30PM, but utilized the beers component and finised around 1:45PM
 import React from 'react'
 import axios from 'axios'
-import { Container, Card, Header, Divider } from 'semantic-ui-react'
+import { Container, Card, Header, Divider, Button } from 'semantic-ui-react'
+import InfiniteScroll from 'react-infinite-scroller'
 
 class Breweries extends React.Component {
-  state = { breweries: [] }
+  state = { breweries: [], page: 1, totalPages: 0 }
 
 componentDidMount() {
     axios.get('/api/all_breweries')
-    .then( res => this.setState({ breweries: res.data.entries }) )
+    .then( res => this.setState({ breweries: res.data.entries, totalPages: res.data.total_pages }) )
 }
-
-
 
 showBreweries = () => {
   return this.state.breweries.map( (brewery, i) => {
@@ -21,20 +20,43 @@ showBreweries = () => {
         <Card.Header>
             {brewery.name}
         </Card.Header>
+        <Card.Description>
+            Description: {brewery.description}
+        </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          {/* Add link to button to show brewery details page */}
+          <Button basic fluid color='green'> View Brewery</Button>
         </Card.Content>
     </Card>
     )
   })
 }
 
+moreBreweriesPlz = () => {
+    axios.get(`/api/all_breweries?page=${this.state.page + 1}`)
+      .then( ({ data }) => {
+        this.setState( state => {
+          return { breweries: [...state.breweries, ...data.entries], page: state.page + 1  }
+        })
+      })
+   }
+
   render() {
     return(
       <Container textAlign='center'>
       <Divider />
         <Header as='h1' color='green'>Brewery List</Header>
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this.moreBreweriesPlz}
+          hasMore={this.state.page < this.state.totalPages}
+          loader={<div className='loader' key={0}>Loading ...</div>}
+        >
         <Card.Group itemsPerRow={4}>
           {this.showBreweries()}
         </Card.Group>
+        </InfiniteScroll>
       </Container>
       )
     }
